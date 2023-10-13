19c5c14b0b68df281c57a99ec2290903f4dff0af
hey <774260818@qq.com>
2023-10-13 19:06:08 +0800

    1

--- githubApi.js
+++ githubApi.js
@@ -1,13 +1,20 @@
+const axios = require('axios');
 const { GITHUB_TOKEN } = require('./env.js');
 
 async function postGitHubComment(repo, commitSha, comment) {
-    await axios.post(`https://api.github.com/repos/${repo}/commits/${commitSha}/comments`, {
-        body: comment
-    }, {
-        headers: {
-            'Authorization': `token ${GITHUB_TOKEN}`
-        }
-    });
+    try {
+        await axios.post(`https://api.github.com/repos/${repo}/commits/${commitSha}/comments`, {
+            body: comment
+        }, {
+            headers: {
+                'Authorization': `token ${GITHUB_TOKEN}`
+            }
+        });
+    } catch (error) {
+        console.error("Error while posting comment on GitHub:", error);
+        // 可选择抛出错误，或者进行其他处理
+        throw new Error("Failed to post comment on GitHub.");
+    }
 }
 
 exports.postGitHubComment = postGitHubComment;
