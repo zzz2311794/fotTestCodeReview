802a05fec1dc9b29ea917667846f7a35031ac3b6
hey <774260818@qq.com>
2023-11-09 21:05:05 +0800

    webhook3.0

--- main.js
+++ main.js
@@ -9,8 +9,12 @@ const { WEBHOOK_SECRET } = require('./env.js');
 
 const app = express();
 
-app.use(express.json());
-
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*'); // 允许所有域名访问
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS');
+    next();
+});
 
 //webhook的主函数
 app.post('/webhook', async (req, res) => {
